cf8d78f90e3a43e097bdf3cee207382145e6d749
Jessica B <185999834+1008-JB@users.noreply.github.com>
2024-10-31 00:14:12 -0600

    Contenido de la app

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-# Título de la aplicación
+# Título de la app
 st.header("Insights en Ventas de Coches: Un Análisis Exploratorio")
 
-# Cargar los datos desde el archivo CSV
+# Leer los datos
 car_data = pd.read_csv(
     r'C:\Users\BETY_\Downloads\Analisis de datos\Sprint 6 Herramientas de desarrollo de software\Proyecto_final\analisis_exploratorio\vehicles_us.csv'
 )
@@ -13,27 +13,23 @@ car_data = pd.read_csv(
 # Crear una casilla de verificación para el histograma
 build_histogram = st.checkbox('Construir un histograma')
 
-# Si la casilla de verificación del histograma está seleccionada
-if build_histogram:
-    # Mostrar mensaje de creación de histograma
+if build_histogram:  # si la casilla de verificación está seleccionada
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     
-    # Crear el histograma usando Plotly
-    fig = px.histogram(car_data, x="odometer")
+    # Crear el histograma
+    fig_hist = px.histogram(car_data, x="odometer")
     
-    # Mostrar el gráfico interactivo en Streamlit
-    st.plotly_chart(fig, use_container_width=True)
+    # Mostrar el gráfico de histograma interactivo
+    st.plotly_chart(fig_hist, use_container_width=True)
 
 # Crear una casilla de verificación para el gráfico de dispersión
-build_scatter = st.checkbox('Construir gráfico de dispersión')
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
 
-# Si la casilla de verificación del gráfico de dispersión está seleccionada
-if build_scatter:
-    # Mostrar mensaje de creación de gráfico de dispersión
-    st.write('Creación de un gráfico de dispersión de precio vs. odómetro')
+if build_scatter:  # si la casilla de verificación está seleccionada
+    st.write('Creación de un gráfico de dispersión para el precio frente al odómetro')
     
-    # Crear el gráfico de dispersión usando Plotly
-    fig = px.scatter(car_data, x="odometer", y="price", title="Precio vs. Odómetro")
+    # Crear el gráfico de dispersión
+    fig_scatter = px.scatter(car_data, x="odometer", y="price", title="Precio vs Odómetro")
     
-    # Mostrar el gráfico interactivo en Streamlit
-    st.plotly_chart(fig, use_container_width=True)
+    # Mostrar el gráfico de dispersión interactivo
+    st.plotly_chart(fig_scatter, use_container_width=True)
